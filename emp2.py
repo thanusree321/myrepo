0d3ed5767ca3aa662c1d810816d125a1ca1e173c
thanu sree <thanusreeparuchuri333@gmail.com>
2022-09-28 22:51:11 +0530

    return matching recs read from emp2 json file

---
+++ emp2.py
@@ -0,0 +1,44 @@
+import json
+with open('emp2.json', "r") as fd:
+    jsondata = fd.read()
+    re = json.loads(jsondata)
+
+def gng(recs, findredc):
+    f = []
+    for item in recs:
+        found = True
+        for k in findredc:
+            if item[k] != findredc[k]:
+                found = False
+                break
+        if found == True:
+            return item
+
+    return None
+
+
+allrecs =[{"fname": "abc01", "lname": "xyz02", "age": "32", "address": "111 xyz rd, abc city, MD, 21774"},
+        {"fname": "abc02", "lname": "xyz02", "age": "32", "address": "112 xyz rd, abc city, MD, 21774"},
+        {"fname": "abc03", "lname": "xyz02", "age": "43", "address": "113 xyz rd, abc city, MD, 21774"},
+        {"fname": "abc03", "lname": "xyz02", "age": "22", "address": "114 xyz rd, abc city, MD, 21774"}
+    ]
+find = {"fname": "abc02", "lname": "xyz02", "age": "32"}
+exp = {"fname": "abc02", "lname": "xyz02", "age": "32", "address": "112 xyz rd, abc city, MD, 21774"}
+
+if gng(allrecs, find) == exp:
+    print("test passed")
+else:
+    print("test failed")
+
+allrecs =[{"fname": "abc01", "lname": "xyz02", "age": "32", "address": "111 xyz rd, abc city, MD, 21774"},
+        {"fname": "abc02", "lname": "xyz02", "age": "32", "address": "112 xyz rd, abc city, MD, 21774"},
+        {"fname": "abc03", "lname": "xyz02", "age": "43", "address": "113 xyz rd, abc city, MD, 21774"},
+        {"fname": "abc03", "lname": "xyz02", "age": "22", "address": "114 xyz rd, abc city, MD, 21774"}
+    ]
+find = {}
+exp = {"fname": "abc03", "lname": "xyz02", "age": "43", "address": "113 xyz rd, abc city, MD, 21774"}
+
+if gng(allrecs, find) != exp:
+    print("test passed")
+else:
+    print("test failed")
