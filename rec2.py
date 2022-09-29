8afe4238c2dbb8c35d362c300e367deb05e962fe
thanu sree <thanusreeparuchuri333@gmail.com>
2022-09-29 15:27:59 +0530

    return matching recs

--- rec2.py
+++ rec2.py
@@ -2,24 +2,41 @@
 #if no matching found,then return empty list
 
 def gng(recs, findredc):
-    A = []
-    for ele in recs:
+    f = []
+    for item in recs:
         found = True
         for k in findredc:
-            if k not in ele or findredc[k] != ele[k]:
+            if item[k] != findredc[k]:
                 found = False
                 break
-        if found:
-           A.append(ele)
-    print(A)
+        if found == True:
+            return item
 
-gng( [{"fname": "abc01", "lname": "xyz02", "age": "32",
-         "address": "111 xyz rd, abc city, MD, 21774"},
-        {"fname": "abc02", "lname": "xyz02", "age": "32",
-         "address": "112 xyz rd, abc city, MD, 21774"},
-        {"fname": "abc02", "lname": "xyz02", "age": "43",
-         "address": "113 xyz rd, abc city, MD, 21774"},
-        {"fname": "abc03", "lname": "xyz02", "age": "22",
-         "address": "114 xyz rd, abc city, MD, 21774"}
-        ]
-, {"fname": "abc02", "lname": "xyz02"})
+    return None
+
+
+allrecs =[{"fname": "abc01", "lname": "xyz02", "age": "32", "address": "111 xyz rd, abc city, MD, 21774"},
+        {"fname": "abc02", "lname": "xyz02", "age": "32", "address": "112 xyz rd, abc city, MD, 21774"},
+        {"fname": "abc03", "lname": "xyz02", "age": "43", "address": "113 xyz rd, abc city, MD, 21774"},
+        {"fname": "abc03", "lname": "xyz02", "age": "22", "address": "114 xyz rd, abc city, MD, 21774"}
+    ]
+find = {"fname": "abc02", "lname": "xyz02", "age": "32"}
+exp = {"fname": "abc02", "lname": "xyz02", "age": "32", "address": "112 xyz rd, abc city, MD, 21774"}
+
+if gng(allrecs, find) == exp:
+    print("test passed")
+else:
+    print("test failed")
+
+allrecs =[{"fname": "abc01", "lname": "xyz02", "age": "32", "address": "111 xyz rd, abc city, MD, 21774"},
+        {"fname": "abc02", "lname": "xyz02", "age": "32", "address": "112 xyz rd, abc city, MD, 21774"},
+        {"fname": "abc03", "lname": "xyz02", "age": "43", "address": "113 xyz rd, abc city, MD, 21774"},
+        {"fname": "abc03", "lname": "xyz02", "age": "22", "address": "114 xyz rd, abc city, MD, 21774"}
+    ]
+find = {}
+exp ={"fname": "abc03", "lname": "xyz02", "age": "43", "address": "113 xyz rd, abc city, MD, 21774"}
+
+if gng(allrecs, find) != exp:
+    print("test passed")
+else:
+    print("test failed")
